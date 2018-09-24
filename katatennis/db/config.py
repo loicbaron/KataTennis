8db08718d6f69e5508e1db2f4cf99d0ea3d22d3d
Loic Baron <loic.baron@lip6.fr>
2018-09-24 15:23:44 +0200

    Share SQLite between different threads

--- katatennis/db/config.py
+++ katatennis/db/config.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 config = {
-    'SQL_DATABASE_URI': 'sqlite:///katatennis/db/database.db',
+    'SQL_DATABASE_URI': 'sqlite:///katatennis/db/database.db?check_same_thread=False',
     'SQL_ISOLATION_LEVEL': 'SERIALIZABLE',
     'SQL_ECHO': True,
     'SQL_ECHO_POOL': False,
     'SQL_CONVERT_UNICODE': True,
-    #'SQL_POOL_SIZE': 5,
-    #'SQL_POOL_TIMEOUT': 30,
+    'SQL_POOL_SIZE': None,
+    'SQL_POOL_TIMEOUT': None,
     #'SQL_POOL_RECYCLE': 3600,
     #'SQL_MAX_OVERFLOW': 10,
     'SQL_AUTOCOMMIT': False,
